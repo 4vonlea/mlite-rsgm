1651f4f162bb5fa30712bb3d617760bdcbafd8bf
unknown <mhmmdalfkr026@gmail.com>
2026-08-09 13:20:56 +0800

    click copy scroll soap -to server

--- plugins/rawat_jalan/js/admin/rawat_jalan.js
+++ plugins/rawat_jalan/js/admin/rawat_jalan.js
@@ -574,6 +574,7 @@ $("#soap").on("click",".copy_soap", function(event){
   $('textarea[name=evaluasi]').val(evaluasi);
   $('input:text[name=spo2]').val(spo2);
 
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
 $("#soap").on("click",".edit_soap", function(event){
