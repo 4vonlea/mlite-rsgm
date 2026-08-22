54113410cc506d382e50c4bc94d3e53b93e58d61
6lack12ose <fjrn.azwry@gmail.com>
2026-08-22 13:54:29 +0800

    umur pasien di OD - done server

--- plugins/oral_diagnostic/Admin.php
+++ plugins/oral_diagnostic/Admin.php
@@ -110,6 +110,12 @@ class Admin extends AdminModule
         foreach ($rows as $row) {
           $bridging_sep = $this->db('bridging_sep')->where('no_rawat', $row['no_rawat'])->oneArray();
           $row['no_sep'] = isset_or($bridging_sep['no_sep']);
+          
+          $tgl_kunjungan = new \DateTime($row['tgl_registrasi']);
+          $tgl_lahir = new \DateTime($row['tgl_lahir']);
+          $diff = $tgl_lahir->diff($tgl_kunjungan);
+          $row['umur_kunjungan'] = $diff->y . ' Tahun ' . $diff->m . ' Bulan ' . $diff->d . ' Hari';
+          
           $this->assign['list'][] = $row;
         }
 
